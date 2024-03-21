55f80a4bcd9feee6bbddf6bbb14ce9d80ec634fc
Nick Galis <nickgalis123@gmail.com>
2024-03-21 15:59:35 -0400

    formatting

--- myshell.c
+++ myshell.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/wait.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 #include <fcntl.h>
 
 #define MAXLINE 1024
@@ -24,16 +24,16 @@ void parser(char* cmdline, char** argv) {
 /* Function to execute user commands */
 void execute(char** argv) {
     int out = 0; // Output redirection flag
-    int fileDescriptor; // File Descriptor for output file if redirection is intended
+    int file_descriptor; // File Descriptor for output file if redirection is intended
     pid_t pid;
     int status;
-    char *outFile = NULL; // Output file name if any
+    char *out_file = NULL; // Output file name if any
 
     // Check if the command contains '>' for redirection
     for (int i = 0; argv[i] != NULL; i++) {
         if (strcmp(argv[i], ">") == 0) {
             argv[i] = NULL; // Remove '>' from command
-            outFile = argv[i + 1]; // The next string in the command is the output file name
+            out_file = argv[i + 1]; // The next string in the command is the output file name
             out = 1;
         }
     }
@@ -46,9 +46,9 @@ void execute(char** argv) {
     }
     else if (pid == 0) {
         if (out) {
-            fileDescriptor = open(outFile, O_WRONLY | O_CREAT, S_IRWXU);
-            dup2(fileDescriptor, STDOUT_FILENO);
-            close(fileDescriptor);
+            file_descriptor = open(out_file, O_WRONLY | O_CREAT, S_IRWXU);
+            dup2(file_descriptor, STDOUT_FILENO);
+            close(file_descriptor);
         }
 
         if (execvp(argv[0], argv) < 0) {
