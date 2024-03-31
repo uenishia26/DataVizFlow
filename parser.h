457e321d873948b7ae07b4edadf82a064ddb4cd1
Nick Galis <nickgalis123@gmail.com>
2024-03-31 12:56:31 -0400

    bug fixes and reformatting

--- parser.h
+++ parser.h
@@ -1,7 +1,3 @@
-//
-// Created by nickgalis on 3/30/24.
-//
-
 #ifndef CS410_PROJECT_2_PARSER_H
 #define CS410_PROJECT_2_PARSER_H
 
@@ -13,68 +9,11 @@ typedef struct command {
 
 } Command;
 
-void trim(char** str) {
-    char* ptr = *str;
-    int len;
+void trim(char** str);
 
-    // Remove leading white-spaces
-    while(isspace((unsigned char)*ptr)) ptr++;
+Command* parser(char* cmdline);
 
-    *str = ptr;
+Command* pipe_parser(char *cmdline);
 
-    // Remove trailing white-spaces
-    len = strlen(ptr);
-    while(len > 0 && isspace((unsigned char)ptr[len-1])) len--;
+#endif
 
-    ptr[len] = '\0';
-}
-
-/* Function to parse user command */
-Command* parser(char* cmdline) {
-    // Count spaces for the number of arguments
-    int spaces = 0;
-    for (int i = 0; cmdline[i]; i++)
-        spaces += (cmdline[i] == ' ');
-
-    // Allocate memory for arguments
-    char **argv = malloc((spaces + 2) * sizeof(char*));
-
-    // Tokenize cmd_line by spaces and store the tokens in argv
-    int idx = 0;
-    char *token = strtok(cmdline, " ");
-    while (token != NULL) {
-        argv[idx++] = token;
-        token = strtok(NULL, " ");
-    }
-    argv[idx] = NULL;
-
-    // Create and return the Command
-    Command *cmd = malloc(sizeof(Command));
-    cmd->argv = argv;
-    cmd->background = (argv[idx - 1] && strcmp(argv[idx - 1], "&") == 0);
-    if (cmd->background) {
-        argv[idx - 1] = NULL; // Remove '&'
-    }
-    cmd->next = NULL;
-
-    return cmd;
-}
-
-Command* pipe_parser(char *cmdline) {
-    Command *head = NULL;
-    Command **node = &head;
-
-    char* saveptr;
-    char* line = strtok_r(cmdline, "|", &saveptr);
-
-    while (line != NULL) {
-        trim(&line);
-        *node = parser(line);
-        node = &((*node)->next);
-        line = strtok_r(NULL, "|", &saveptr);
-    }
-
-    return head;
-}
-
-#endif //CS410_PROJECT_2_PARSER_H
--- signals.h
+++ signals.h
@@ -1,7 +1,3 @@
-//
-// Created by nickgalis on 3/30/24.
-//
-
 #ifndef CS410_PROJECT_2_SIGNALS_H
 #define CS410_PROJECT_2_SIGNALS_H
 
@@ -15,25 +11,13 @@
 
 #define MAXLINE 1024
 
-pid_t childpids[MAXLINE];
-int childpids_size = 0;
-
-void sigint_handler(int signum) {
-    int i;
-    for(i = 0; i < childpids_size; i++) {
-        if(childpids[i] != 0) {
-            kill(childpids[i], SIGINT);
-        }
-    }
-    printf("\nStopped all foreground processes.\n");
-    printf("myshell> ");
-    fflush(stdout);
-}
-
-void sigchld_handler(int signum) {
-    // Wait for any child without blocking
-    while (waitpid((pid_t)(-1), 0, WNOHANG) > 0) {}
-}
+extern int childpids_size;
+extern pid_t childpids[];
 
 
-#endif //CS410_PROJECT_2_SIGNALS_H
+void sigint_handler(int signum);
+
+void sigchld_handler(int signum);
+
+
+#endif
